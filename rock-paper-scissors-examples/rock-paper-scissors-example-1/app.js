088ed7cbae6b2735d11f82fb00bb78fd093795f3
Alberto <61217370+dayofthetech@users.noreply.github.com>
2022-08-24 18:11:30 -0500

    Update

--- rock-paper-scissors-examples/rock-paper-scissors-example-1/app.js
+++ rock-paper-scissors-examples/rock-paper-scissors-example-1/app.js
@@ -13,9 +13,13 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
   getResult()
 }))
 
+//function to generate a random number
 function generateComputerChoice() {
+  //Math.floor gives you a full integer
   const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
-  
+  //instead of hard coding it as 3, for later you can add more choices.
+
+//assigning a number from the random and a choice, so if the number is 1, then it will print rock
   if (randomNumber === 1) {
     computerChoice = 'rock'
   }
@@ -25,9 +29,11 @@ function generateComputerChoice() {
   if (randomNumber === 3) {
     computerChoice = 'paper'
   }
+  //here it will be displayed the choice
   computerChoiceDisplay.innerHTML = computerChoice
 }
 
+// function to individually compared each choice
 function getResult() {
   if (computerChoice === userChoice) {
     result = 'its a draw!'
@@ -50,5 +56,7 @@ function getResult() {
   if (computerChoice === 'scissors' && userChoice === "paper") {
     result = 'you lose!'
   }
+
+  //display the resutl. .innerHTML add the result into the webpage
   resultDisplay.innerHTML = result
 }
